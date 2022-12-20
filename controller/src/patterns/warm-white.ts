ecdb3a1f827fdadef7013fd40b07b3e37afeb731
Louis Orleans <louis@orleans.io>
2022-12-20 00:50:16 -0800

    💄 change warm white color

--- controller/src/patterns/warm-white.ts
+++ controller/src/patterns/warm-white.ts
@@ -4,7 +4,7 @@ import { rgb_to_int } from '../utils/colorspaces.js';
 import type { Pattern } from './pattern.interface.js';
 
 export class WarmWhitePattern implements Pattern {
-  static #warm_white = rgb_to_int(255, 180, 107);
+  static #warm_white = rgb_to_int(255, 142, 33);
 
   constructor(
     private leds_service: LEDSService,
